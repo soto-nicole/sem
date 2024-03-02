86142dbdf8500ad75678495e6ad3b6afe54cea45
Nicole Soto <40626148@live.napier.ac.uk>
2024-03-02 16:15:35 +0000

    Expand SQL query: Get job title, salary, department, and manager

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -83,17 +83,21 @@ public class App
         }
     }
 
-    public Employee getEmployee(int ID)
-    {
-        try
-        {
+    public Employee getEmployee(int ID) {
+        try {
             // Create an SQL statement
             Statement stmt = con.createStatement();
-            // Create string for SQL statement
-            String strSelect =
-                    "SELECT emp_no, first_name, last_name "
-                            + "FROM employees "
-                            + "WHERE emp_no = " + ID;
+            // SQL query to fetch the required details
+            String strSelect = "SELECT e.emp_no, e.first_name, e.last_name, t.title, s.salary, d.dept_name, m.first_name AS manager_first_name, m.last_name AS manager_last_name " +
+                    "FROM employees e " +
+                    "JOIN titles t ON e.emp_no = t.emp_no AND t.to_date = '9999-01-01' " +
+                    "JOIN salaries s ON e.emp_no = s.emp_no AND s.to_date = '9999-01-01' " +
+                    "JOIN dept_emp de ON e.emp_no = de.emp_no AND de.to_date = '9999-01-01' " +
+                    "JOIN departments d ON de.dept_no = d.dept_no " +
+                    "LEFT JOIN dept_manager dm ON dm.dept_no = d.dept_no AND dm.to_date = '9999-01-01' " +
+                    "LEFT JOIN employees m ON dm.emp_no = m.emp_no " +
+                    "WHERE e.emp_no = " + ID;
+
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
             // Return new employee if valid.
@@ -104,9 +108,12 @@ public class App
                 emp.emp_no = rset.getInt("emp_no");
                 emp.first_name = rset.getString("first_name");
                 emp.last_name = rset.getString("last_name");
+                emp.title = rset.getString("title");
+                emp.salary = rset.getInt("salary");
+                emp.dept_name = rset.getString("dept_name");
+                emp.manager = rset.getString("manager_first_name") + " " + rset.getString("manager_last_name");
                 return emp;
-            }
-            else
+            } else
                 return null;
         }
         catch (Exception e)
